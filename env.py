b182630eb8bd66e18d92107e5adf0560bb95d473
dagmara-szproch <dagmamara@gmail.com>
2026-02-27 08:56:39 +0000

    Remove env.py from tracking and update .gitignore

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "SECRET_KEY", "django-insecure-y156regz6h+8b=870-w)ifvyzi6=7*-w_-9@iq#xq&64bc1ai6"
-)
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_DreKmNqlg71k@ep-curly-sea-a2fnasdb.eu-central-1.aws.neon.tech/tart_sniff_truce_461688"
-)
